4cdc43c4d826580df8339a013bf0614c5c662ad1
Luke Day <ld227707@falmouth.ac.uk>
2020-06-28 17:39:11 +0100

    Added two classes: IK Bone, which will be on every bone in the body, and IK Controller which will be on the root bone of the body (and controls most of the functionality of the IK System).

---
+++ Assets/Scripts/IKBone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class IKBone : MonoBehaviour
+{
+    [Header("IK Bone")]
+    public float boneLength;
+    public Vector3 jointPosition;
+
+    protected virtual void Awake()
+    {
+        boneLength = this.GetComponent<BoxCollider>().bounds.size.x;
+        jointPosition = GetJointPosition();
+    }
+
+    protected Vector3 GetJointPosition()
+    {
+        Vector3 jointPos = this.transform.position - (transform.right * (boneLength / 2));
+        return jointPos;
+    }
+}
---
+++ Assets/Scripts/IKController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+// The gameObject which holds this controller will be the root.
+public class IKController : IKBone
+{
+    [Header("IK Controller")]
+    public Vector3 rootPosition; // also known as p0
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rootPosition = jointPosition;
+    }
+}
