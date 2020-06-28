ec3ade207cae356c310c6018976bcc74001b5d7e
Luke Day <ld227707@falmouth.ac.uk>
2020-06-28 18:24:02 +0100

    Made a recursive method to retrieve all IKBone components from children of children, and made a GetTotalBodyLength method which is now used in the CanReachTarget method.

--- Assets/Scripts/IKController.cs
+++ Assets/Scripts/IKController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 
 // The gameObject which holds this controller will be the root.
 public class IKController : IKBone
@@ -6,17 +8,47 @@ public class IKController : IKBone
     [Header("IK Controller")]
     public Vector3 rootPosition; // also known as p0
     public Transform target;
+    public List<IKBone> allBones;
 
     protected override void Awake()
     {
         base.Awake();
         rootPosition = jointPosition;
+    }
+
+    private void Start()
+    {
+        Debug.LogFormat("Total length of body is: {0}", GetTotalBodyLength());
         Debug.LogFormat("You {0} reach the target, currently.", CanReachTarget() ? "can" : "cannot");
     }
 
     private bool CanReachTarget()
     {
         float distance  = (target.position - rootPosition).magnitude;
-        return distance <= boneLength;
+        return distance <= GetTotalBodyLength();
+    }
+
+    private float GetTotalBodyLength()
+    {
+        GetAllBoneChildren(this.transform.parent); // Populates allBones list
+
+        float length = 0;
+        allBones.ForEach(bone => length += bone.boneLength);
+        return length;
+    }
+
+    private void GetAllBoneChildren(Transform parent)
+    {
+        if (!parent) return;
+
+        foreach (Transform child in parent)
+        {
+            if (!child) continue;
+
+            IKBone childBone = child.GetComponent<IKBone>();
+            if (childBone) allBones.Add(childBone);
+
+            GetAllBoneChildren(child);
+        }
     }
 }
