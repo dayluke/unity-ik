0d4716ec7c06748a90ca7447f0f0b3d5b7db1a87
Luke Day <ld227707@falmouth.ac.uk>
2020-06-28 17:50:19 +0100

    Added a check for whether a single bone (the root) can reach the target or not.

--- Assets/Scripts/IKController.cs
+++ Assets/Scripts/IKController.cs
@@ -5,10 +5,18 @@ public class IKController : IKBone
 {
     [Header("IK Controller")]
     public Vector3 rootPosition; // also known as p0
+    public Transform target;
 
     protected override void Awake()
     {
         base.Awake();
         rootPosition = jointPosition;
+        Debug.LogFormat("You {0} reach the target, currently.", CanReachTarget() ? "can" : "cannot");
+    }
+
+    private bool CanReachTarget()
+    {
+        float distance  = (target.position - rootPosition).magnitude;
+        return distance <= boneLength;
     }
 }
